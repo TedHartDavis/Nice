d89d6e6f35d889ed2cca0245a7c4c923640ade2a
Edward Hart-Davis <yugiteddy@gmail.com>
2018-04-22 07:33:10 +0100

    Added command-line argument support allowing for evil mode and calling of rudimentary help menu

--- Nice.c
+++ Nice.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main ()
+int main (int argc, char *argv[])
 {
+	char** to_print;
+	if (argc < 2)
+	{
+		to_print = "Nice\n";
+	}
+	else
+	{
+		if (strcmp (argv[1], "--evil") == 0)
+		{
+			to_print = "nice\n";
+		}
+		else if (strcmp (argv[1], "-h") == 0 || strcmp (argv[1], "--help") == 0)
+		{
+			printf("No argument: Outputs 'Nice'\nFirst argument as '-h' or '--help': Display this screen\nFirst argument as '--evil': Ouputs 'nice'\n");
+			return 0;
+		}
+		else
+		{
+			printf ("Unknown argument. Run with '-h' for help.\n");
+			return 1;
+		}
+	}
 	for(;;)
 	{
-	printf ("Nice\n");
+	printf (to_print);
 	}
 	return 0;
 }
