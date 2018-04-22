a6fa83e53b5821c3234629065dd15c8f07563f36
Edward Hart-Davis <yugiteddy@gmail.com>
2018-04-22 07:47:10 +0100

    make build.sh by default quieter with verbose option, add check for built file existing and complain otherwise; add shebang

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
-gcc Nice.c -o Nice
-chmod +x Nice
+#! /bin/bash
+if [ "$1" == "--verbose" ] || [ "$1" == "-v" ]; then
+	gcc Nice.c
+else
+	gcc Nice.c -0 Nice >& /dev/null
+fi
+
+if [ ! -f "Nice" ]; then
+	echo "Failed to produce 'Nice'"
+else
+	chmod +x "Nice"
+fi
+
